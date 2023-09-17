7c04b6d6db88064bb251316ea75d57c6738617a4
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-09-17 11:46:07 -0300

    Add video transcription handler

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscription } from "./routes/create-transcription";
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
-app.register(uploadVideoRoute)
+app.register(uploadVideoRoute);
+app.register(createTranscription);
 
 app.listen({
   port: 3333
